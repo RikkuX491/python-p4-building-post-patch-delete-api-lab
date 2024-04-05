e911319660e6b5a21fb0b462c6bdbe0ce96f8a27
Ricardo Guerra <RikkuG491@gmail.com>
2024-04-05 15:53:26 -0400

    Added solution code

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb
 
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
@@ -21,14 +22,23 @@ def home():
 @app.route('/bakeries')
 def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
-    return make_response(  bakeries,   200  )
+    return make_response(bakeries, 200)
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+
+    if(request.method == 'GET'):
+        bakery_serialized = bakery.to_dict()
+        return make_response (bakery_serialized, 200)
+    elif(request.method == 'PATCH'):
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+        
+        db.session.commit()
+
+        response_body = bakery.to_dict()
+        return make_response(response_body, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -44,6 +54,30 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods', methods=['POST'])
+def add_baked_good():
+    new_baked_good = BakedGood(name=request.form.get('name'), price=request.form.get('price'), bakery_id=request.form.get('bakery_id'))
+    db.session.add(new_baked_good)
+    db.session.commit()
+    return make_response(new_baked_good.to_dict(), 201)
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = db.session.get(BakedGood, id)
+    
+    if(baked_good):
+        db.session.delete(baked_good)
+        db.session.commit()
+        response_body = {
+            "message": "record successfully deleted"
+        }
+        return make_response(response_body, 200)
+    else:
+        response_body = {
+            "error": "Baked Good Not Found!"
+        }
+        return make_response(response_body, 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
